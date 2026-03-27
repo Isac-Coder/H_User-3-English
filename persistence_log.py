d3abf8aa31c8d49771b7358365c0976e2c32bde7
Isac-Coder <isaacalvarez0516@gmail.com>
2026-03-27 18:37:31 -0500

    added validations

--- persistence_log.py
+++ persistence_log.py
@@ -1,4 +1,5 @@
 import os
+WHITE = '\033[1;37m'
 
 if os.path.exists("database.txt"):
     pass
@@ -9,30 +10,47 @@ else:
 with open("database.txt", "r") as file:
     contador = len(file.readlines())
 
-os.system('cls' if os.name == 'nt' else 'clear')
+
 while True:
-    option = int(input("Enter an option:\n#"))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f"{WHITE}MENU\n")
+    print("Choose 1 for the Daily Blocker")
+    print("Choose 2 to display the daily blockers")
+    print("Choose 3 to clean the database")
+    print("Choose 4 to exit the program or exit for close the program\n")
+
+    option = input("Enter an option:\n# ")
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    if option == 1:
-        print("\nWrite to exit to close the program")
-        blocker = input("\nEnter your daily blocker:\n# ")
+    if option == "1":
+        blocker = input("\nEnter your Daily Blocker:\n# ")
         contador += 1
         with open ("database.txt", "a") as file:
             file.write(f"{contador} : {blocker}\n")
 
-    elif option == 2:
-        with open ("database.txt", "r") as file:
-            for line in file:
-                print(f"{line.strip()}")
-        print("\nWrite to exit to close the program")
+    elif option == "2":
+        if os.path.getsize("database.txt") == 0:
+            print("The database is empty\n")
+            input("Press enter to continue...")
+        else:
+            with open ("database.txt", "r") as file:
+
+                for line in file:
+                    print(f"{line.strip()}")
+            input("Press enter to continue...")
     
-    elif option == 3:
+    elif option == "3":
         with open("database.txt", "w") as archivo:
             pass 
         print("The database was cleaned correctly\n")
         input("Press enter to continue...")
         os.system('cls' if os.name == 'nt' else 'clear')
     
-    elif option == 4:
+    elif option == "4":
         break
+
+    elif option == "exit":
+        break
+    
+    else:
+        print("Invalid option\n")
